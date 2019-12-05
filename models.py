eb9aeaa911f3532024e906845b5590ce02a86039
root <dwightfoster03@gmail.com>
2019-12-05 22:33:38 +0000

    new commit

--- models.py
+++ models.py
@@ -20,14 +20,50 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
-        self.conv2 = nn.Conv2d(32,64,3,1)
-        self.conv3 = nn.Conv2d(64,128,2)
-        self.conv4 = nn.Conv2d(128,64,2)
-        self.pool  = nn.MaxPool2d(2,2)
-        self.dropout = nn.Dropout(0.4)
-        self.fc1 = nn.Linear(64*26*26,100)
-        self.fc2 =nn.Linear(100,136)
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 32, 5),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Dropout(0.6),
+            
+            nn.Conv2d(32,64,3,1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+            nn.Dropout(0.6),
+            
+            nn.Conv2d(64,128,2),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+            nn.Dropout(0.6),          
+       
+            nn.Conv2d(128,64,2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Dropout(0.6),
+            
+            nn.Conv2d(64,64,3,padding=1),
+            nn.MaxPool2d(2,2),
+            nn.ReLU(),
+            nn.Dropout(0.6)
+
+            )
+        self.classifier = nn.Sequential(
+            nn.Linear(64*26*26,2048),
+            nn.ReLU(),
+            nn.Dropout(0.4),
+            
+            nn.Linear(2048,1204),
+            nn.ReLU(),
+            nn.Dropout(0.4),
+            
+            nn.Linear(1204,100),
+            nn.ReLU(),
+            nn.Dropout(0.4),
+            
+            nn.Linear(100,136)
+        )
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
         
@@ -37,13 +73,8 @@ class Net(nn.Module):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.pool(x))
-        x = F.relu(self.conv2(x))
-        x = self.pool(F.relu(self.conv3(x)))
-        x = self.pool(F.relu(self.conv4(x)))
+        x = self.conv(x)
         x = x.view(x.size(0),-1)
-        x = self.dropout(self.fc1(x))
-        x = self.fc2(x)
+        x = self.classifier(x)
         # a modified x, having gone through all the layers of your model, should be returned
         return x
